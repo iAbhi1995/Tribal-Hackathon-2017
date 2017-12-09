0c0f91f7af22cdde3acc57f7cd9e2e12e5cf3884
ayush <ayushjain17aug@gmail.com>
2017-12-09 21:21:38 +0530

    Multi Language Feature Added

--- app/src/main/java/com/tribal/hackathon/tribalhackathon17/MainActivity.java
+++ app/src/main/java/com/tribal/hackathon/tribalhackathon17/MainActivity.java
@@ -1,6 +1,10 @@
 package com.tribal.hackathon.tribalhackathon17;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
@@ -8,6 +12,8 @@ import android.view.MenuItem;
 import android.widget.TextView;
 
 import com.tribal.hackathon.tribalhackathon17.Helper.BottomNavigationViewHelper;
+
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,14 +28,15 @@ public class MainActivity extends AppCompatActivity {
                 case R.id.navigation_home:
                     mTextMessage.setText(R.string.title_home);
                     return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
-                    return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
+                case R.id.navigation_scheme:
+                    mTextMessage.setText(R.string.title_scheme);
                     return true;
                 case R.id.navigation_account:
-                    mTextMessage.setText(R.string.title_notifications);
+                    mTextMessage.setText(R.string.title_account);
+                    return true;
+                case R.id.navigation_settings:
+                    Intent intent = new Intent(MainActivity.this,Settings.class);
+                    startActivity(intent);;
                     return true;
             }
             return false;
@@ -41,12 +48,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setTitle(R.string.app_name);
 
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
-
         BottomNavigationViewHelper.disableShiftMode(navigation);
+
+        SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(this);
+        Configuration config = getBaseContext().getResources().getConfiguration();
+
+        String lang = settings.getString("LANG", "");
+        if (! "".equals(lang) && ! config.locale.getLanguage().equals(lang)) {
+            Locale locale = new Locale(lang);
+            Locale.setDefault(locale);
+            config.locale = locale;
+            getBaseContext().getResources().updateConfiguration(config, getBaseContext().getResources().getDisplayMetrics());
+        }
+
     }
 
 }
